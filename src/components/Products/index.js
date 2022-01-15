ae71029cc6d6a11f3b8e43f27086d5eb7b6cf7fd
DrStone <drstoneluffy@gmail.com>
2022-01-15 23:44:01 +0500

    Changes on 1/15/2022

---
+++ src/components/Products/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Product from './Product'
+import { useFetch } from '../customhook/2-useFetch'
+import styled from 'styled-components'
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-prop-types-example'
+
+const Index = () => {
+  const { products } = useFetch(url)
+  return (
+    <CustomDiv> 
+      <CustomHeading>Products</CustomHeading>
+      <section className='products'>
+        {products.map((product) => {
+          return <Product key={product.id} {...product} />
+        })}
+      </section>
+    </CustomDiv>
+  )
+}
+const CustomHeading = styled.h1`
+//   border:2px solid green;
+  text-transform: uppercase;
+  padding-x: 3px;
+  font-size:42px;
+`
+const CustomDiv = styled.div`
+  // border:2px solid red;
+  margin-top:100px;
+  
+`
+export default Index
