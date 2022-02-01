7aa896297fc2438e5aa95661e1cb6e675dfdbbf3
Hamza Ali Khan <85096025+ErenYea@users.noreply.github.com>
2022-02-01 22:18:37 +0500

    Update table.js

--- src/components/AdminPanel/table.js
+++ src/components/AdminPanel/table.js
@@ -9,56 +9,78 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import styled from 'styled-components'
+
+
+import { tableCellClasses } from '@mui/material/TableCell';
+
+
+
 
 
 const Tables = () =>{
     const {loading,products} = useFetch('http://localhost:3001/table');
-    console.log(products[0]);
+    // console.log(products);
    
 
     if (loading){
         return (
-            <div className="container">
+            <div className="container ">
                 <Box sx={{ display: 'flex' , justifyContent: 'center'}}>
                     <CircularProgress />
                 </Box>
             </div>
         )
     } else {
-        console.log(products[0].data[0]);
+        // console.log(products[0].data[0]);
         return (
-            <div className="review">
+            <CustomDiv className="container review">
                 
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 850 }} aria-label="simple table">
                         <TableHead>
                         <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell align="right">Type</TableCell>
+                            <TableCell>Item Number</TableCell>
+                            <TableCell align="right">Product title</TableCell>
+                            <TableCell align="right">Product url</TableCell>
+                            <TableCell align="right">image url</TableCell>
+                            <TableCell align="right">price</TableCell>
                             
                         </TableRow>
                         </TableHead>
                         <TableBody>
                         {products[0].data[0].map((row) => (
                             <TableRow
-                            key={row.name}
+                            key={row.item_number}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                             <TableCell component="th" scope="row">
-                                {row.name}
+                                {row.item_number}
                             </TableCell>
-                            <TableCell align="right">{row.type}</TableCell>
+                            <TableCell align="right">{row.product_title}</TableCell>
+                            <TableCell align="right">{row.product_url}</TableCell>
+                            <TableCell align="right">{row.image_url}</TableCell>
+                            <TableCell align="right">{row.price}</TableCell>
                             
                             </TableRow>
                         ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
-            </div>
+            </CustomDiv>
         )
     }
 
 
 
 }
+
+
+const CustomDiv = styled.div`
+  width:2000px;
+  max-width:1239px;
+//   padding: 0 0;
+  
+
+`
 export default Tables;
